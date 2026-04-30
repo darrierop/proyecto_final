bcdedc87ecffbbfc17af34786be433cff4b3698c
darrierop <davidarri2004@gmail.com>
2026-04-30 11:06:33 +0200

    No SQL

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# Entrypoint: establece SERVER_NAME con el puerto dinámico de Railway
 export SERVER_NAME=":${PORT:-80}"
 echo ">> Iniciando FrankenPHP en $SERVER_NAME"
 exec frankenphp run --config /etc/caddy/Caddyfile --adapter caddyfile
